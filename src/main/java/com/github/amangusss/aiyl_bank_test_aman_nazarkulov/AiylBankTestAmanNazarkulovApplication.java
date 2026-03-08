2cbe91436fd107b9d694fe22f9c52a98a1e52020
amangusss <aman.nazarkulov@gmail.com>
2026-03-09 02:05:01 +0600

    chore: initial commit

---
+++ src/main/java/com/github/amangusss/aiyl_bank_test_aman_nazarkulov/AiylBankTestAmanNazarkulovApplication.java
@@ -0,0 +1,13 @@
+ package com.github.amangusss.aiyl_bank_test_aman_nazarkulov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiylBankTestAmanNazarkulovApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiylBankTestAmanNazarkulovApplication.class, args);
+    }
+
+}
